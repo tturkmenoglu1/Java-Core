be12b8ed2e40449dedcb08fd966e6b89b93c8ce1
Taha Turkmenoglu <106612619+tturkmenoglu1@users.noreply.github.com>
2022-10-01 13:24:14 +0400

    Add files via upload

---
+++ myJavaPracticesEng/Question1.java
@@ -0,0 +1,35 @@
+package myJavaPracticesEng;
+
+import java.util.Scanner;
+
+public class Question1 {
+    public static void main(String[] args) {
+
+        /*
+        ask user to enter his/her name, lastname and age
+         */
+
+        Scanner scan=new Scanner(System.in);
+
+        System.out.println("Please enter your name, lastname and age. \nPress enter after each input");
+        String name=scan.next();
+        String lastName=scan.next();
+        int age=scan.nextInt();
+        System.out.println("name : "+ name + "\nlastName : " + lastName + "\nage : " + age);
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+    }
+}
