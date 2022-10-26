4021167ef980980a73ebc8159f37913aa6808ce7
Taha Turkmenoglu <106612619+tturkmenoglu1@users.noreply.github.com>
2022-10-26 22:24:47 +0400

    Add files via upload

---
+++ myJavaPracticesEng/Question8.java
@@ -0,0 +1,19 @@
+package myJavaPracticesEng;
+
+public class Question8 {
+    public static void main(String[] args) {
+        // While using String methods
+        // Take “  Learning java123 is fun@  ”  String and
+        // Convert to this “Learning java is fun.”
+
+        String str = "  Learning java123 is fun@  ";
+
+        str = str.trim();
+        str = str.replaceAll("\\s", "space");
+        str = str.replaceAll("\\W", "");
+        str = str.replaceAll("\\d", "");
+        str = str.replaceAll("space", " ");
+        str = str.replaceAll("C", "c");
+        System.out.println(str);
+    }
+}
