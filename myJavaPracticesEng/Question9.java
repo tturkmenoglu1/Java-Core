73360f90dc9448ad3a65951d2f6c90c51cafc59a
Taha Turkmenoglu <106612619+tturkmenoglu1@users.noreply.github.com>
2022-10-27 21:23:21 +0400

    Add files via upload

---
+++ myJavaPracticesEng/Question9.java
@@ -0,0 +1,26 @@
+package myJavaPracticesEng;
+
+import java.util.Scanner;
+
+public class Question9 {
+    public static void main(String[] args) {
+        // The task is finding the area and perimeter of indicated side lengths of rectangle
+        /* Example: Please enter the side lengths of rectangle
+             Short length: 4
+             Long length: 5
+             Area: 20
+             Perimeter: 18
+         */
+
+        Scanner scan = new Scanner(System.in);
+        System.out.print("Please enter the side lengths of rectangle \nShort length: ");
+        int shortSide=scan.nextInt();
+        System.out.print("Long length: ");
+        int longSide=scan.nextInt();
+        int area=shortSide*longSide;
+        int perimeter=2*(shortSide+longSide);
+        System.out.println("Area: "+area);
+        System.out.println("Perimeter: "+perimeter);
+
+    }
+}
