a11924b810f0316bc3b36fd2516cbda4f3646848
Taha Turkmenoglu <106612619+tturkmenoglu1@users.noreply.github.com>
2022-10-30 22:22:45 +0400

    Add files via upload

---
+++ myJavaPracticesEng/Question12.java
@@ -0,0 +1,12 @@
+package myJavaPracticesEng;
+
+public class Question12 {
+    public static void main(String[] args) {
+        int sum = 0;
+        int[] numbers = {1, 2, 3, 4, 5, 6, 7, 8, 9, 10};
+        for (int i = 0; i < numbers.length; i++) {
+            sum += numbers[i];
+        }
+        System.out.println("Sum of elements of array: " + sum);
+    }
+}
