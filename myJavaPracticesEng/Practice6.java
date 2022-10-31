c5b3e75d78b3a7f901adf63f5fc65d57f066d75b
Taha Turkmenoglu <106612619+tturkmenoglu1@users.noreply.github.com>
2022-10-31 22:13:42 +0400

    Update Practice6.java

--- myJavaPracticesEng/Practice6.java
+++ myJavaPracticesEng/Practice6.java
@@ -2,7 +2,7 @@ package myJavaPracticesEng;
 
 import java.util.Scanner;
 
-public class Practice6 {
+public class Practice06 {
     /*
             Ask from client to enter a sentence.
             If the sentence contains "upper" word the make whole letter of sentence upper case,
