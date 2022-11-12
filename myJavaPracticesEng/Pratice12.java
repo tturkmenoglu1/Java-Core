0d96611253ac891656f369a786c97e4e07d712c4
Taha Turkmenoglu <106612619+tturkmenoglu1@users.noreply.github.com>
2022-11-12 22:24:45 +0400

    Update and rename Question12.java to Pratice12.java

--- myJavaPracticesEng/Pratice12.java
+++ myJavaPracticesEng/Pratice12.java
@@ -1,6 +1,6 @@
 package myJavaPracticesEng;
 
-public class Question12 {
+public class Practice12 {
     public static void main(String[] args) {
         int sum = 0;
         int[] numbers = {1, 2, 3, 4, 5, 6, 7, 8, 9, 10};
