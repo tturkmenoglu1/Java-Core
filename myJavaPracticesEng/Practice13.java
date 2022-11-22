5b5a771e5d0a7df3ef5453979e7fed499803213c
Taha Turkmenoglu <106612619+tturkmenoglu1@users.noreply.github.com>
2022-11-22 20:48:31 +0400

    Update and rename A13.java to Practice13.java

---
+++ myJavaPracticesEng/Practice13.java
@@ -0,0 +1,13 @@
+package myJavaPractices;
+
+public class Practice13 {
+    public static void main(String[] args) {
+        int avg=0;
+        int [] nums={1, 2, 3, 4, 5, 6, 7, 8, 9, 10};
+        for (int i = 0; i <nums.length ; i++) {
+            avg+=nums[i];
+        }
+        avg/= nums.length;
+        System.out.println("Array sum: "+avg);
+    }
+}
